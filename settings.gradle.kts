960098d836a726306f1ebf2106360d4eda5098a0
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2026-02-26 22:20:32 -0600

    Update heap space setting, reduce to 90 modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -91,14 +91,4 @@ include(
     "module-88",
     "module-89",
     "module-90",
-    "module-91",
-    "module-92",
-    "module-93",
-    "module-94",
-    "module-95",
-    "module-96",
-    "module-97",
-    "module-98",
-    "module-99",
-    "module-100"
 )
